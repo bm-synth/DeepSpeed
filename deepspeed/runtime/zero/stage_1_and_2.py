d50c8300861d3b7d594e120281411fd52bb14337
Bruno Magalhaes <bruno.magalhaes@synthesia.io>
2025-03-03 01:23:47 +0000

    Fix expert grad scaling problem with ZeRO optimizer (#6546)
    Fix [#6545]
    
    work:
    - expert gradient average: divide edp_world_size -> divide dp_world_size
    - unit test: make sure model with different dp/ep has same expert
    gradient
    
    ---------
    
    Co-authored-by: wangyiou <wangyiou@xiaohongshu.com>
    Co-authored-by: Masahiro Tanaka <81312776+tohtana@users.noreply.github.com>
    Co-authored-by: Logan Adams <114770087+loadams@users.noreply.github.com>

--- deepspeed/runtime/zero/stage_1_and_2.py
+++ deepspeed/runtime/zero/stage_1_and_2.py
@@ -1071,14 +1071,10 @@ class DeepSpeedZeroOptimizer(ZeROOptimizer):
                 param = self.bit16_groups[i][param_idx_in_group]
 
                 process_group = self.dp_process_group
-                grad_reduc = self.get_gradient_for_reduction(param)
-                #Averages gradients at parameter level if ipg has a moe param
-                #Otherwise averaging is done at the entire buffer level at the end of the loop
-                # MoE param have different groups
+
                 if self.ipg_bucket_has_moe_params:
                     process_group = self.expert_dp_process_group[param.group_name] if is_moe_param(
                         param) else self.dp_process_group
-                    grad_reduc.data.div_(dist.get_world_size(group=process_group) / float(self.sequence_parallel_size))
 
                 partition_ids = self.param_to_partition_ids[i][param_id]
                 assert all([p_id < dist.get_world_size(group=process_group) for p_id in partition_ids
@@ -1117,8 +1113,7 @@ class DeepSpeedZeroOptimizer(ZeROOptimizer):
                     curr_size += numel
                     prev_id, prev_process_group = partition_id, process_group
 
-            if not self.ipg_bucket_has_moe_params:
-                tensor.div_(dist.get_world_size(group=self.dp_process_group) / float(self.sequence_parallel_size))
+            tensor.div_(dist.get_world_size(group=self.dp_process_group) / float(self.sequence_parallel_size))
 
             buckets = {}
             for i, (dst, bucket_offset, numel) in enumerate(rank_and_offsets):
