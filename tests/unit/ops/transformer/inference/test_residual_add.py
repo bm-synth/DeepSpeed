26638e48e0d1927d88ec80949437ca714578239f
Bruno Magalhaes <bruno.magalhaes@synthesia.io>
2025-03-03 01:10:29 +0000

    Remove dtype(fp16) condition check for residual_add unit test (#5329)
    When the dtype is bf16 or fp32 the if condition is not satisfied and it
    continues execution instead of skipping when triton is not installed.
    
    Co-authored-by: Shaik Raza Sikander <srsikander@habana.ai>
    Co-authored-by: Logan Adams <114770087+loadams@users.noreply.github.com>
    Co-authored-by: Olatunji Ruwase <olruwase@microsoft.com>
    Signed-off-by: Bruno Magalhaes <bruno.magalhaes@synthesia.io>

--- tests/unit/ops/transformer/inference/test_residual_add.py
+++ tests/unit/ops/transformer/inference/test_residual_add.py
@@ -72,10 +72,10 @@ def run_residual_add_reference(hidden_state, residual, attn_output, attn_bias, f
 @pytest.mark.parametrize("mp_size", [1, 2])
 @pytest.mark.parametrize("pre_attn_norm", [True, False])
 @pytest.mark.parametrize("use_triton_ops", [True, False])
-def test_residual_add(batch, sequence, hidden_dim, dtype, mlp_after_attn, add_bias, mp_size, pre_attn_norm,
-                      use_triton_ops):
-    if not deepspeed.get_accelerator().is_triton_supported():
-        pytest.skip("triton is not supported on this system")
+def test_residual_add(inference_module, batch, sequence, hidden_dim, dtype, mlp_after_attn, add_bias, mp_size,
+                      pre_attn_norm, use_triton_ops):
+    if not deepspeed.HAS_TRITON and use_triton_ops:
+        pytest.skip("triton has to be installed for the test")
     ds_out = torch.randn((batch, sequence, hidden_dim), dtype=dtype, device=get_accelerator().device_name())
     residual = torch.randn((batch, sequence, hidden_dim), dtype=dtype, device=get_accelerator().device_name())
     attn_output = torch.randn((batch, sequence, hidden_dim), dtype=dtype, device=get_accelerator().device_name())
