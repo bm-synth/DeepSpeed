fdadf27ecc2ff0adecc063fc78cf10dbd3df067b
Bruno Magalhaes <bruno.magalhaes@synthesia.io>
2025-03-03 01:14:24 +0000

    Fix for fragmented linear inputs in ZeRO 3 Linear layers where reshap… (#881)
    Co-authored-by: Olatunji Ruwase <olruwase@microsoft.com>
    Co-authored-by: Jeff Rasley <jerasley@microsoft.com>

--- deepspeed/runtime/zero/partition_parameters.py
+++ deepspeed/runtime/zero/partition_parameters.py
@@ -327,7 +327,34 @@ class InsertPostInitMethodToModuleSubClasses(object):
             global top_level_context
             top_level_context = self
 
-        zero_init_context += 1
+            return wrapper
+
+        def _enable_class(cls):
+            cls._old_init = cls.__init__
+            cls.__init__ = partition_after(cls.__init__)
+
+        def _init_subclass(cls, **kwargs):
+            cls.__init__ = partition_after(cls.__init__)
+
+        # Replace .__init__() for all existing subclasses of torch.nn.Module
+        for subclass in torch.nn.modules.module.Module.__subclasses__():
+            _enable_class(subclass)
+
+        # holding on to the current __init__subclass__ for exit
+        torch.nn.modules.module.Module._old_init_subclass = torch.nn.modules.module.Module.__init_subclass__
+        torch.Tensor.__old_new__ = torch.Tensor.__new__
+
+        # Replace .__init__() for future subclasses of torch.nn.Module
+        torch.nn.modules.module.Module.__init_subclass__ = classmethod(_init_subclass)
+        torch.Tensor.__new__ = new_cuda_tensor
+        torch.empty = empty_cuda_tensor
+
+        if self.mem_efficient_linear:
+            print_rank_0(
+                f"Your linear layers are being patched with more memory efficient version. This will persit unless manually reset.",
+                force=True)
+            self.linear_bk = torch.nn.functional.linear
+            torch.nn.functional.linear = LinearFunctionForZeroStage3.apply
 
     def __exit__(self, exc_type, exc_value, traceback):
         if not self.enabled:
@@ -342,11 +369,15 @@ class InsertPostInitMethodToModuleSubClasses(object):
             global top_level_context
             top_level_context = None
 
-            if dist.get_rank() == 0:
-                billion_elems = InsertPostInitMethodToModuleSubClasses.num_module_elements / 1e9
-                num_params = InsertPostInitMethodToModuleSubClasses.num_module_parameters
-                logger.info(
-                    f"finished initializing model - num_params = {num_params}, num_elems = {billion_elems:.2f}B")
+        # Replace .__init__() for future subclasses of torch.nn.Module
+        torch.nn.modules.module.Module.__init_subclass__ = torch.nn.modules.module.Module._old_init_subclass
+
+        torch.Tensor.__new__ = torch.Tensor.__old_new__
+        torch.empty = _orig_torch_empty
+
+        #un doing it here will undo it during training
+        #if self.mem_efficient_linear:
+        #    torch.nn.functional.linear = self.linear_bk
 
         # Now that we cleaned up the metaclass injection, raise the exception.
         if exc_type is not None:
@@ -1084,6 +1115,13 @@ class Init(InsertPostInitMethodToModuleSubClasses):
 
                 assert ds_config.zero_config.offload_param.nvme_path is not None, \
                 f'"nvme_path" in DeepSpeed Config cannot be None if remote device is {OffloadDeviceEnum.nvme}'
+
+        if mem_efficient_linear:
+            print_rank_0(
+                f"Your linear layers are being patched with more memory efficient version. This will persit unless manually turned reset.",
+                force=True)
+            self.linear_bk = torch.nn.functional.linear
+            torch.nn.functional.linear = LinearFunctionForZeroStage3.apply
 
     def _post_init_method(self, module):
         #see_memory_usage(f"Before converting params in {module.__class__.__name__}", force=False)
