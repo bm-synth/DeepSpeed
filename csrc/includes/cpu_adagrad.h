84a6368c81e2b42a836e01da62dc8c795e6b8040
Bruno Magalhaes <bruno.magalhaes@synthesia.io>
2025-03-03 01:15:41 +0000

    CPU-Adam: Fix compile Issue (#1537)
    * fixing the softmax masking when using triangular masking
    
    * move the TILE declaration outside of the SIMD loop
    
    * remove unrelated changes
    
    * fix Adagrad compile issue

--- csrc/includes/cpu_adagrad.h
+++ csrc/includes/cpu_adagrad.h
@@ -1,43 +1,54 @@
-// Copyright (c) Microsoft Corporation.
-// SPDX-License-Identifier: Apache-2.0
-
-// DeepSpeed Team
-
 #pragma once
 
-#define NOMINMAX  // Windows idiosyncrasy
-                  // https://stackoverflow.com/questions/4913922/possible-problems-with-nominmax-on-visual-c
-
+#include <cuda_fp16.h>
+#include <cuda_runtime_api.h>
 #include <stdio.h>
-#include <torch/extension.h>
 #include <cassert>
+#include "cuda.h"
+#include "custom_cuda_layers.h"
 #include "simd.h"
 
-#define STEP(SPAN)                                                           \
-    template <typename ds_params_precision_t, typename ds_state_precision_t> \
-    void Step_##SPAN(ds_params_precision_t* _params,                         \
-                     ds_params_precision_t* grads,                           \
-                     ds_state_precision_t* _exp_avg_sq,                      \
-                     size_t _param_size);
+#define STEP(SPAN)                                \
+    void Step_##SPAN(float* _params,              \
+                     float* grads,                \
+                     float* _exp_avg_sq,          \
+                     size_t _param_size,          \
+                     __half* dev_param = nullptr, \
+                     bool half_precision = false);
 
 class Adagrad_Optimizer {
 public:
     Adagrad_Optimizer(float alpha = 1e-2, float eps = 1e-8, float weight_decay = 0)
-        : _alpha(alpha), _eps(eps), _weight_decay(weight_decay)
+        : _alpha(alpha), _eps(eps), _weight_decay(weight_decay), _buf_index(false)
     {
+        cudaMallocHost((void**)_doubled_buffer, TILE * sizeof(float));
+        cudaMallocHost((void**)(_doubled_buffer + 1), TILE * sizeof(float));
+
+        _streams[0] = Context::Instance().GetCurrentStream();
+        _streams[1] = Context::Instance().GetNewStream();
     }
-    ~Adagrad_Optimizer() {}
+    ~Adagrad_Optimizer()
+    {
+        cudaFreeHost(_doubled_buffer[0]);
+        cudaFreeHost(_doubled_buffer[1]);
+    }
 #if defined(__AVX512__) or defined(__AVX256__)
-    template <int span, typename ds_params_precision_t, typename ds_state_precision_t>
+    template <int span>
     void Step_AVX(size_t* rounded_size,
-                  ds_params_precision_t* _params,
-                  ds_params_precision_t* grads,
-                  ds_state_precision_t* _exp_avg_sq,
-                  size_t param_size);
+                  float* _params,
+                  float* grads,
+                  float* _exp_avg_sq,
+                  size_t param_size,
+                  __half* dev_param = nullptr,
+                  bool half_precision = false);
 #endif
     STEP(1)
     STEP(4)
     STEP(8)
+    inline void SynchronizeStreams()
+    {
+        for (int i = 0; i < 2; i++) cudaStreamSynchronize(_streams[i]);
+    }
     inline void IncrementStep(size_t step)
     {
         _step++;
@@ -58,22 +69,23 @@ private:
     float _betta1_t;
     float _betta2_t;
     size_t _step;
+
+    float* _doubled_buffer[2];
+    bool _buf_index;
+
+    cudaStream_t _streams[2];
 };
 
 #if defined(__AVX512__) or defined(__AVX256__)
-template <int span, typename ds_params_precision_t, typename ds_state_precision_t>
+template <int span>
 void Adagrad_Optimizer::Step_AVX(size_t* rounded_size,
-                                 ds_params_precision_t* _params,
-                                 ds_params_precision_t* grads,
-                                 ds_state_precision_t* _exp_avg_sq,
-                                 size_t _param_size)
+                                 float* _params,
+                                 float* grads,
+                                 float* _exp_avg_sq,
+                                 size_t _param_size,
+                                 __half* dev_params,
+                                 bool half_precision)
 {
-#if !defined(__AVX512__)
-    if (std::is_same_v<ds_params_precision_t, c10::BFloat16> ||
-        std::is_same_v<ds_state_precision_t, c10::BFloat16>) {
-        return;
-    }
-#endif
     size_t new_rounded_size = 0;
     AVX_Data eps_4;
     eps_4.data = SIMD_SET(_eps);
@@ -89,19 +101,20 @@ void Adagrad_Optimizer::Step_AVX(size_t* rounded_size,
         size_t copy_size = TILE;
         if ((t + TILE) > new_rounded_size) copy_size = new_rounded_size - t;
         size_t offset = copy_size + t;
+        if ((t / TILE) >= 2) { cudaStreamSynchronize(_streams[_buf_index]); }
 #pragma omp parallel for
         for (size_t i = t; i < offset; i += SIMD_WIDTH * span) {
             AVX_Data grad_4[span];
-            simd_load<span>(grad_4, grads + i);
+            simd_load<span>(grad_4, grads + i, half_precision);
 
             AVX_Data momentum_4[span];
-            simd_load<span>(momentum_4, grads + i);
+            simd_load<span>(momentum_4, grads + i, false);
 
             AVX_Data variance_4[span];
-            simd_load<span>(variance_4, _exp_avg_sq + i);
+            simd_load<span>(variance_4, _exp_avg_sq + i, false);
 
             AVX_Data param_4[span];
-            simd_load<span>(param_4, _params + i);
+            simd_load<span>(param_4, _params + i, half_precision);
 
             if (_weight_decay > 0) { simd_fma<span>(grad_4, param_4, weight_decay4, grad_4); }
 
@@ -111,8 +124,22 @@ void Adagrad_Optimizer::Step_AVX(size_t* rounded_size,
             simd_div<span>(grad_4, momentum_4, grad_4);
             simd_fma<span>(param_4, grad_4, step_size_4, param_4);
 
-            simd_store<span>(_params + i, param_4);
-            simd_store<span>(_exp_avg_sq + i, variance_4);
+            simd_store<span>(_params + i, param_4, half_precision);
+            if (dev_params) {
+                simd_store<span>(_doubled_buffer[_buf_index] + (i - t), param_4, half_precision);
+            }
+            simd_store<span>(_exp_avg_sq + i, variance_4, false);
+        }
+
+        if (dev_params) {
+            if (half_precision)
+                launch_param_update_half(
+                    _doubled_buffer[_buf_index], dev_params + t, copy_size, _streams[_buf_index]);
+            else
+                launch_param_update(
+                    _doubled_buffer[_buf_index], dev_params + t, copy_size, _streams[_buf_index]);
+
+            _buf_index = !_buf_index;
         }
     }
     *rounded_size = new_rounded_size;
