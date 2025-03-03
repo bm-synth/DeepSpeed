8945920872c910e12eee288cb721a51a19940947
Bruno Magalhaes <bruno.magalhaes@synthesia.io>
2025-03-03 01:11:48 +0000

    Cleanup ops/transformer/inference tests (#6830)

--- tests/unit/ops/transformer/inference/test_bias_gelu.py
+++ tests/unit/ops/transformer/inference/test_bias_gelu.py
@@ -10,6 +10,7 @@ from deepspeed.accelerator import get_accelerator
 from deepspeed.ops.op_builder import InferenceBuilder
 from deepspeed.ops.transformer import DeepSpeedInferenceConfig
 from deepspeed.ops.transformer.inference.op_binding.bias_gelu import BiasGeluOp
+from deepspeed.utils.torch import required_torch_version
 from .inference_test_utils import allclose, get_dtypes
 
 if not deepspeed.ops.__compatible_ops__[InferenceBuilder.NAME]:
