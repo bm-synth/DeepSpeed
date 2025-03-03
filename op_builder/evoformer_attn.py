0ddd9b1d23798c7b40119b1f8a9c9aaf32c71c1b
Bruno Magalhaes <bruno.magalhaes@synthesia.io>
2025-03-03 01:11:02 +0000

    Log operator warnings only in verbose mode (#5917)
    Signed-off-by: Bruno Magalhaes <bruno.magalhaes@synthesia.io>

--- op_builder/evoformer_attn.py
+++ op_builder/evoformer_attn.py
@@ -52,19 +52,10 @@ class EvoformerAttnBuilder(CUDAOpBuilder):
             if verbose:
                 self.warning("Please specify the CUTLASS repo directory as environment variable $CUTLASS_PATH")
             return False
-        if os.path.exists(f'{self.cutlass_path}/CHANGELOG.md'):
-            with open(f'{self.cutlass_path}/CHANGELOG.md', 'r') as f:
-                if '3.1.0' not in f.read():
-                    if verbose:
-                        self.warning("Please use CUTLASS version >= 3.1.0")
-                    return False
-        else:
-            # pip install nvidia-cutlass package
-            try:
-                import cutlass
-            except ImportError:
+        with open(f'{self.cutlass_path}/CHANGELOG.md', 'r') as f:
+            if '3.1.0' not in f.read():
                 if verbose:
-                    self.warning("Please pip install nvidia-cutlass if trying to pre-compile kernels")
+                    self.warning("Please use CUTLASS version >= 3.1.0")
                 return False
             cutlass_major, cutlass_minor = cutlass.__version__.split('.')[:2]
             cutlass_compatible = (int(cutlass_major) >= 3 and int(cutlass_minor) >= 1)
