2be8f54b871b2244d83f580fbdde459ca470402c
Bruno Magalhaes <bruno.magalhaes@synthesia.io>
2025-03-03 01:18:44 +0000

    Fix issue between our abstract accelerator and colossalai's version of op_builder (#2963)
    Co-authored-by: Logan Adams <114770087+loadams@users.noreply.github.com>

--- accelerator/cuda_accelerator.py
+++ accelerator/cuda_accelerator.py
@@ -308,7 +308,7 @@ class CUDA_Accelerator(DeepSpeedAccelerator):
         try:
             # is op_builder from deepspeed or a 3p version? this should only succeed if it's deepspeed
             # if successful this also means we're doing a local install and not JIT compile path
-            from op_builder import __deepspeed__  # noqa: F401 # type: ignore
+            from op_builder import __deepspeed__  # noqa: F401
             return "op_builder"
         except ImportError:
             return "deepspeed.ops.op_builder"
