5574b75d3508971914e509136ca014de02a5e672
Bruno Magalhaes <bruno.magalhaes@synthesia.io>
2025-03-03 01:20:15 +0000

    Allow user to select name of .deepspeed_env (#4006)
    * formatting
    
    * Formatting
    
    * arg formatting
    
    * Allow user to select file
    
    * Add docs
    
    * Switch environment variable name to  to match other existing envvars
    
    * Handle if the user enters a path or a filename
    
    * Clarify in docs that you can enter either a file or a path

--- deepspeed/launcher/runner.py
+++ deepspeed/launcher/runner.py
@@ -30,7 +30,7 @@ from deepspeed.accelerator import get_accelerator
 DLTS_HOSTFILE = "/job/hostfile"
 EXPORT_ENVS = ['MLFLOW', 'NCCL', 'PYTHON', 'MV2', 'UCX']
 EXPORT_ENVS += NEBULA_EXPORT_ENVS
-DEEPSPEED_ENVIRONMENT_NAME = ".deepspeed_env"
+DEEPSPEED_ENVIRONMENT_NAME = os.getenv("DS_ENV_FILE", ".deepspeed_env")
 DEEPSPEED_ENVIRONMENT_PATHS = [os.path.expanduser("~"), '.']
 PDSH_MAX_FAN_OUT = 1024
 
@@ -223,15 +223,18 @@ def parse_args(args=None):
                         "arguments.")
 
     parser.add_argument('user_args', nargs=argparse.REMAINDER)
+
     parser.add_argument("--bind_cores_to_rank",
                         action="store_true",
                         help="Bind each rank to different cores of the host")
+
     parser.add_argument("--bind_core_list",
                         type=str,
                         default=None,
                         help="List of cores to bind to with comma separated list of "
                         "numbers and range. i.e. 1,3-5,7 => [1,3,4,5,7].  When not "
                         "specified, all cores on system would be used rank binding")
+
     return parser.parse_args(args=args)
 
 
@@ -571,7 +574,10 @@ def main(args=None):
                     runner.add_export(var, env[var])
 
         for environ_path in DEEPSPEED_ENVIRONMENT_PATHS:
-            environ_file = os.path.join(environ_path, DEEPSPEED_ENVIRONMENT_NAME)
+            environ_file = DEEPSPEED_ENVIRONMENT_NAME
+            # handle if users to enter path for `DS_ENV_FILE`
+            if not os.path.isfile(environ_file):
+                environ_file = os.path.join(environ_path, DEEPSPEED_ENVIRONMENT_NAME)
             if os.path.isfile(environ_file):
                 logger.info(f"deepspeed_env file = {environ_file}")
                 with open(environ_file, 'r') as fd:
