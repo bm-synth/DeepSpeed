25e8cbbf384786e5750d946f82e5ba63e8d3bd09
Bruno Magalhaes <bruno.magalhaes@synthesia.io>
2025-03-03 01:18:49 +0000

    fix return prev key and value , added strides to from_blob (#2828)
    Co-authored-by: Mor Zusman <morz@ai21.com>
    Co-authored-by: Olatunji Ruwase <olruwase@microsoft.com>
    Co-authored-by: Jeff Rasley <jerasley@microsoft.com>

--- csrc/transformer/inference/csrc/pt_binding.cpp
+++ csrc/transformer/inference/csrc/pt_binding.cpp
@@ -578,24 +578,22 @@ std::vector<at::Tensor> ds_softmax_context(at::Tensor& query_key_value,
                                InferenceContext::Instance().GetCurrentStream(false),
                                1);
 
-    if (layer_id == num_layers - 1) InferenceContext::Instance().advance_tokens();
-    auto prev_key = torch::from_blob(
-        workspace + offset,
-        {bsz, heads, all_tokens, k},
-        {hidden_dim * static_cast<int64_t>(InferenceContext::Instance().GetMaxTokenLength()),
-         k * static_cast<int64_t>(InferenceContext::Instance().GetMaxTokenLength()),
-         k,
-         1},
-        options);
+    if (layer_id == num_layers - 1) Context::Instance().advance_tokens();
+    auto prev_key = torch::from_blob(workspace + offset,
+                                     {bsz, heads, all_tokens, k},
+                                     {hidden_dim * Context::Instance().GetMaxTokenLenght(),
+                                      k * Context::Instance().GetMaxTokenLenght(),
+                                      k,
+                                      1},
+                                     options);
 
-    auto prev_value = torch::from_blob(
-        workspace + offset + value_offset,
-        {bsz, heads, all_tokens, k},
-        {hidden_dim * static_cast<int64_t>(InferenceContext::Instance().GetMaxTokenLength()),
-         k * static_cast<int64_t>(InferenceContext::Instance().GetMaxTokenLength()),
-         k,
-         1},
-        options);
+    auto prev_value = torch::from_blob(workspace + offset + value_offset,
+                                       {bsz, heads, all_tokens, k},
+                                       {hidden_dim * Context::Instance().GetMaxTokenLenght(),
+                                        k * Context::Instance().GetMaxTokenLenght(),
+                                        k,
+                                        1},
+                                       options);
 
     return {output, prev_key, prev_value};
 }
