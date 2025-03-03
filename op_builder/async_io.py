fc3df5fbf4593c61eee488e6d7ab7364e80eb3ce
Bruno Magalhaes <bruno.magalhaes@synthesia.io>
2025-03-03 01:15:05 +0000

    aio: note the reason for compiling with -O0 (#1249)
    Co-authored-by: Olatunji Ruwase <olruwase@microsoft.com>

--- op_builder/async_io.py
+++ op_builder/async_io.py
@@ -49,6 +49,7 @@ class AsyncIOBuilder(TorchCPUOpBuilder):
         return ['csrc/aio/py_lib', 'csrc/aio/common'] + CUDA_INCLUDE
 
     def cxx_args(self):
+        # -O0 for improved debugging, since performance is bound by I/O
         CPU_ARCH = self.cpu_arch()
         SIMD_WIDTH = self.simd_width()
         return [
