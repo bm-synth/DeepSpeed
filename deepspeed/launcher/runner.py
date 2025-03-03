0d20b4ee5bf4b91c84d580a214cd4575edd0d420
Bruno Magalhaes <bruno.magalhaes@synthesia.io>
2025-03-03 01:14:15 +0000

    Fix regression in runner (#843)
    Signed-off-by: Bruno Magalhaes <bruno.magalhaes@synthesia.io>

--- deepspeed/launcher/runner.py
+++ deepspeed/launcher/runner.py
@@ -499,7 +499,10 @@ def main(args=None):
     # encode world info as base64 to make it easier to pass via command line
     world_info_base64 = encode_world_info(active_resources)
 
-    multi_node_exec = (args.force_multi or len(active_resources) > 1) and not args.no_ssh
+    multi_node_exec = len(active_resources) > 1
+
+    if multi_node_exec and not shutil.which('pdsh'):
+        raise RuntimeError("pdsh is not installed, unable to proceed")
 
     if not multi_node_exec:
         deepspeed_launch = [
