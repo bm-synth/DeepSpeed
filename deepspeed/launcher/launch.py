f9ba74e8d238d21164d5a9a7fda618c5cfa72233
Bruno Magalhaes <bruno.magalhaes@synthesia.io>
2025-03-03 01:13:52 +0000

    Update launcher to set local rank environ variable (#597)
    * Update launch.py
    
    * formatting

--- deepspeed/launcher/launch.py
+++ deepspeed/launcher/launch.py
@@ -201,7 +201,11 @@ def main():
                     current_env[key] = val
 
     processes = []
-    cmd = []
+    for local_rank in range(0, num_local_procs):
+        # each process's rank
+        dist_rank = global_rank_mapping[local_node][local_rank]
+        current_env["RANK"] = str(dist_rank)
+        current_env["LOCAL_RANK"] = str(local_rank)
 
     if not args.enable_elastic_training:
         if args.enable_each_rank_log != "None":
