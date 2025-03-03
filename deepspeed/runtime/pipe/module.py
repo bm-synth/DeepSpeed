a95af8e7163296561200093f119de5b7bc3d34d5
Bruno Magalhaes <bruno.magalhaes@synthesia.io>
2025-03-03 01:14:34 +0000

    Fix all Pipeline Module Parameters being sent to cuda:0 (#687)

--- deepspeed/runtime/pipe/module.py
+++ deepspeed/runtime/pipe/module.py
@@ -193,7 +193,7 @@ class PipelineModule(nn.Module):
         self.global_rank = dist.get_rank(group=self.world_group)
         self.world_size = dist.get_world_size(group=self.world_group)
         self.local_rank = int(os.environ.get("LOCAL_RANK", None))
-        assert self.local_rank is not None
+        assert self.local_rank != None
 
         if topology:
             self._topo = topology
@@ -241,7 +241,7 @@ class PipelineModule(nn.Module):
 
         #with torch.random.fork_rng(devices=[get_accelerator().current_device_name()]):
         self._build()
-        self.to(get_accelerator().device_name(self.local_rank))
+        self.to(f'cuda:{self.local_rank}')
 
         self.tied_comms = self._index_tied_modules()
         self._synchronize_tied_weights()
