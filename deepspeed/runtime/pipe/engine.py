36d2f9956d65dbd0071f0287ce00c586317409cd
Bruno Magalhaes <bruno.magalhaes@synthesia.io>
2025-03-03 01:21:05 +0000

    pipe engine _aggregate_total_loss: more efficient loss concatenation (#4327)
    * _aggregate_total_loss: more efficient loss concatenation
    
    optimize _aggregate_total_loss function in order to remove dependancy
    of copying from device to host and back to device.
    This reduce the runtime on the host.
    
    * Fixing the if/else block on which the optimization should take place
    
    ---------
    
    Co-authored-by: Olatunji Ruwase <olruwase@microsoft.com>
    Signed-off-by: Bruno Magalhaes <bruno.magalhaes@synthesia.io>

--- deepspeed/runtime/pipe/engine.py
+++ deepspeed/runtime/pipe/engine.py
@@ -593,7 +593,7 @@ class PipelineEngine(DeepSpeedEngine):
                          for name in self.agg_additional_losses.keys()})
 
             assert self.global_rank in self.grid.pp_group
-            losses = torch.Tensor([self.dp_group_loss, agg_loss]).to(self.device)
+            losses = torch.stack([self.dp_group_loss, agg_loss])
             if self.is_pipe_parallel:
                 dist.broadcast(tensor=losses, src=self.global_rank, group=self.mpu.get_pipe_parallel_group())
         else:
