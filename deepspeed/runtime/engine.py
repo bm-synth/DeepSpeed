1987053205a6ce85224575f02726c74a8c6cb0f9
Bruno Magalhaes <bruno.magalhaes@synthesia.io>
2025-03-03 01:15:43 +0000

    Fix zinf none swapper (#1550)

--- deepspeed/runtime/engine.py
+++ deepspeed/runtime/engine.py
@@ -1522,6 +1522,7 @@ class DeepSpeedEngine(Module):
                 self.module,
                 optimizer,
                 timers=timers,
+                ds_config=self.config,
                 static_loss_scale=self.loss_scale(),
                 dynamic_loss_scale=self.dynamic_loss_scale(),
                 dynamic_loss_args=self.dynamic_loss_scale_args(),
